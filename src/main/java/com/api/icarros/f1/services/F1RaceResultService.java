87da16daf9db5f9ddda356d0fcc681ce4fe5f4ee
Matheus Tabares <tabares.lopes@gmail.com>
2021-02-27 12:09:22 -0300

    implemented find all f1 race result service

---
+++ src/main/java/com/api/icarros/f1/services/F1RaceResultService.java
@@ -0,0 +1,11 @@
+package com.api.icarros.f1.services;
+
+import java.util.List;
+
+import com.api.icarros.f1.domains.F1RaceResult;
+
+public interface F1RaceResultService {
+
+    List<F1RaceResult> findAll();
+
+}
---
+++ src/main/java/com/api/icarros/f1/services/impl/F1RaceResultServiceImpl.java
@@ -0,0 +1,19 @@
+package com.api.icarros.f1.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.api.icarros.f1.domains.F1RaceResult;
+import com.api.icarros.f1.services.F1RaceResultService;
+
+@Service
+public class F1RaceResultServiceImpl implements F1RaceResultService {
+
+    @Override
+    public List<F1RaceResult> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

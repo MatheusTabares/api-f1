89f714cb4d668cffabb8bdfc9e10fd1f13c7cdc8
Matheus Tabares <tabares.lopes@gmail.com>
2021-02-27 11:16:12 -0300

    iniciando projeto

---
+++ src/test/java/com/api/icarros/f1/F1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.icarros.f1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8074ab40c56b1b067349e4e21d87ed43e3681a4a
Matheus Tabares <tabares.lopes@gmail.com>
2021-02-27 11:42:29 -0300

    add f1 race result controller

---
+++ src/main/java/com/api/icarros/f1/controller/F1RaceResultController.java
@@ -0,0 +1,10 @@
+package com.api.icarros.f1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/f1RaceResult")
+public class F1RaceResultController {
+
+}

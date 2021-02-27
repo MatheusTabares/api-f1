7ac7bb3f73d174be2ce4fe4b98ff0ffc75b98df4
Matheus Tabares <tabares.lopes@gmail.com>
2021-02-27 17:37:52 -0300

    find f1 race result find by year

--- src/main/java/com/api/icarros/f1/controllers/F1RaceResultController.java
+++ src/main/java/com/api/icarros/f1/controllers/F1RaceResultController.java
@@ -15,7 +15,7 @@ import com.api.icarros.f1.dtos.F1RaceResultDTO;
 import com.api.icarros.f1.services.F1RaceResultService;
 
 @RestController
-@RequestMapping(value = "/f1")
+@RequestMapping(value = "/api/f1")
 public class F1RaceResultController {
 
     @Autowired
